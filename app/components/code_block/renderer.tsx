2106db4d5560427f58b6dc3a66896524cdd10851
Mike <goldy13_1992@hotmail.co.uk>
2023-11-19 22:07:01 +0700

    tried to fix compile error

--- app/components/code_block/renderer.tsx
+++ app/components/code_block/renderer.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ICodeState from './ICodeState'
 
 
 export default function CodeRenderer({codeState} : {codeState: ICodeState}) :  React.ReactNode {
 
-    //const [currentRenderIdx, setCurrentRenderIdx] = useState(codeState.currentIndex)
-    const renders = [
-        ( <></>),
-        (
-            <div><span className='text-gray-800 dark:text-gray-200'>Hello World</span></div>
-        ),
-
-        (
-            <div className='flex h-full flex-col justify-center'>
-                <div className='w-full flex flex-row justify-center'>
-                    <span className='text-gray-800 dark:text-gray-200'>Hello World</span>
-                </div>
+    let rendererIdx = codeState.canRender ? codeState.currentIndex + 1 : codeState.currentIndex
+ 
+    switch(rendererIdx) {
+        case 0: return (
+            <></>
+        )
+        case 1: return (
+            <div>
+                <span className='text-gray-800 dark:text-gray-200'>
+                    Hello World
+                </span>
             </div>
         )
-    ]
-    let rendererIdx = codeState.canRender ? codeState.currentIndex + 1 : codeState.currentIndex
-
-    return renders[rendererIdx]
+        default: return (
+            <div key={2} className='flex h-full flex-col justify-center'>
+            <div className='w-full flex flex-row justify-center'>
+                <span className='text-gray-800 dark:text-gray-200'>Hello World</span>
+            </div>
+        </div>
+        )
+    }
 }
